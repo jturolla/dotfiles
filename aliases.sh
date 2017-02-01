f6d89e27d927cc2d5976b3ce8ab3d90b827d6e46
Julio Turolla <julio.turolla@gmail.com>
2017-02-01 04:17:44 -0200

    More aliases and other configs

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,14 @@
 alias l='ls -lah'
+
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../../'
+alias .2='cd ../../'
+alias ....='cd ../../../'
+alias .3='cd ../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+alias .6='cd ../../../../../../'
+
+alias s='git status'
--- bash_profile
+++ bash_profile
@@ -1,1 +1,8 @@
-source ~/dotfiles/aliases.sh
+. ~/dotfiles/aliases.sh
+. ~/dotfiles/colors.sh
+
+export EDITOR="vim"
+export CLICOLOR="auto"
+export GREP_OPTIONS='--color=auto'
+export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33'
+export LS_OPTS='--color=auto'
