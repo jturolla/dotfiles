6b9257201f8ff94e91f09987388a10f0fc012bc5
Julio Turolla <julio.turolla@gmail.com>
2017-02-01 03:57:58 -0200

    Organizing and deciding for bash

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.tmux.conf  ~/.tmux.conf
-ln -s ~/dotfiles/.vimrc      ~/.vimrc
-ln -s ~/dotfiles/.zshrc      ~/.zshrc
-ln -s ~/dotfiles/.gitconfig  ~/.gitconfig
-ln -s ~/dotfiles/.gitignore  ~/.gitignore
+ln -s ~/dotfiles/tmux.conf    ~/.tmux.conf
+ln -s ~/dotfiles/vimrc        ~/.vimrc
+ln -s ~/dotfiles/zshrc        ~/.zshrc
+ln -s ~/dotfiles/gitconfig    ~/.gitconfig
+ln -s ~/dotfiles/gitignore    ~/.gitignore
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
 
 ln -sf ~/Dropbox/Downloads/Current/ ~/Downloads
