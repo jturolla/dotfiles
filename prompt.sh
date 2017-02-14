36f3b27effdfd1360e318819f2aafa80799ee3d2
Julio Turolla <julio.turolla@gmail.com>
2017-02-14 00:17:24 -0200

    Refactoring

--- prompt.sh
+++ prompt.sh
@@ -7,5 +7,6 @@ parse_git_branch() {
 user="\[$COLOR_YELLOW\]\u"
 path="\[$COLOR_LIGHT_CYAN\]\w"
 git="\[$COLOR_LIGHT_PURPLE\]\$(parse_git_branch)"
+prompt="\[$COLOR_NC\]\$ "
 
-export PS1="$user $path $git \[$COLOR_NC\]\$ " 
+export PS1="$user $path $git $prompt" 
