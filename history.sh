bc1563efb413b629a9868b050721124837408f99
Julio Turolla <julio.turolla@gmail.com>
2018-03-17 12:09:18 -0300

    some tweeks

---
+++ history.sh
@@ -0,0 +1,5 @@
+# bash shared history {
+#  export HISTCONTROL=ignoredups:erasedups
+#  shopt -s histappend
+#  export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+# }
--- prompt.sh
+++ prompt.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
 git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
+  git branch 2> /dev/null | gsed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
 }
 
-git_emoji()  {
-  emoji="$(yellow Personal)"
-
+git_commiter()  {
   if [ "$(git config user.email)" == "$GIT_WORK_EMAIL" ]; then
-    emoji="$(purple Nubank)"
+    echo "$(purple Nubank)"
+    return
   fi
 
-  echo "$emoji "
+	echo "$(yellow Personal)"
 }
 
 git_prompt() {
-  echo "\$(black $(git_branch)) $(black @) \$(git_emoji)"
+  echo " \$(black $(git_branch)) $(black @) "
 }
 
-export PS1="${green}\u${end} ${purple}\w${end} $(git_prompt) $ "
-
-# bash shared history {
-#  export HISTCONTROL=ignoredups:erasedups
-#  shopt -s histappend
-#  export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
-# }
+export PS1="${green}\u${end} ${purple}\w${end}$(git_prompt)\$(git_commiter) $ "
