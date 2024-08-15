f59ed95df01db3beac9aab1abe4627ab9566aeac
Julio Turolla <j@julioturolla.com>
2024-08-15 09:05:58 -0300

    Path cleanup

--- path.sh
+++ path.sh
@@ -1,13 +1,7 @@
 export PATH="$PATH:/usr/local/sbin"
 export PATH="$PATH:/usr/local"
 export PATH="$PATH:$DOTFILES/bin"
-#export PATH="$PATH:/usr/local/opt/google-cloud-sdk/bin"
 export PATH="$PATH:$HOME/dev/nu/nucli"
-#export PATH="$PATH:$HOME/dev/nu/"
-
 export PATH="$PATH:$HOME/.cargo/bin"
-
-#export PATH="$PATH:$HOME/.pub-cache/bin"
-
-export PATH="$PATH:$GOPATH/bin"
-export PATH="$PATH:/usr/local/opt/go@1.18/bin"
+export PATH="$PATH:/opt/homebrew/bin"
+export PATH="$PATH:$(go env GOPATH)/bin"
