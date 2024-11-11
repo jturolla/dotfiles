0baf7bd39886ea29d162284c61c61611179d1a36
Julio Turolla <jturolla@ju-mb-air.local>
2024-11-11 07:49:54 -0300

    Homebrew auto install

--- nix/flake.nix
+++ nix/flake.nix
@@ -6,12 +6,15 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Homebrew
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
     # Home Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ...}:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager, ...}:
   let
     configuration = { pkgs, ... }: {
 
@@ -111,6 +114,8 @@
           "visual-studio-code"
           "spotify"
           "iterm2"
+          "1password"
+          "arc"
         ];
 
         masApps = {
@@ -176,7 +181,26 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."ju-mb-air" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration
+      modules = [ 
+        # The configuration from above
+        configuration
+        
+        # Homebrew
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "jturolla";
+          };
+        }
+       
+        # Home Manager
         home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
