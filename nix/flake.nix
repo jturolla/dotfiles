462f81f323d62e27d3ce9bcde28e5c1666355110
Julio Turolla <j@julioturolla.com>
2024-11-16 19:22:58 -0300

    Apple defaults

--- nix/flake.nix
+++ nix/flake.nix
@@ -151,6 +151,53 @@
         NSGlobalDomain.InitialKeyRepeat = 12;
         # key repeat speed
         NSGlobalDomain.KeyRepeat = 1;
+
+        CustomUserPreferences = {
+          "com.apple.finder" = {
+            ShowExternalHardDrivesOnDesktop = true;
+            ShowHardDrivesOnDesktop = true;
+            ShowMountedServersOnDesktop = true;
+            ShowRemovableMediaOnDesktop = true;
+            _FXSortFoldersFirst = true;
+            # When performing a search, search the current folder by default
+            FXDefaultSearchScope = "SCcf";
+          };
+          "com.apple.desktopservices" = {
+            # Avoid creating .DS_Store files on network or USB volumes
+            DSDontWriteNetworkStores = true;
+            DSDontWriteUSBStores = true;
+          };
+          "com.apple.screensaver" = {
+            # Require password immediately after sleep or screen saver begins
+            askForPassword = 1;
+            askForPasswordDelay = 0;
+          };
+          "com.apple.screencapture" = {
+            location = "~/Desktop";
+            type = "png";
+          };
+          "com.apple.AdLib" = {
+            allowApplePersonalizedAdvertising = false;
+          };
+          "com.apple.print.PrintingPrefs" = {
+            # Automatically quit printer app once the print jobs complete
+            "Quit When Finished" = true;
+          };
+          "com.apple.SoftwareUpdate" = {
+            AutomaticCheckEnabled = true;
+            # Check for software updates daily, not just once per week
+            ScheduleFrequency = 1;
+            # Download newly available updates in background
+            AutomaticDownload = 1;
+            # Install System data files & security updates
+            CriticalUpdateInstall = 1;
+          };
+          "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+          # Prevent Photos from opening automatically when devices are plugged in
+          "com.apple.ImageCapture".disableHotPlug = true;
+          # Turn on app auto-update
+          "com.apple.commerce".AutoUpdate = true;
+        };
       };
 
       system.keyboard.enableKeyMapping = true;
