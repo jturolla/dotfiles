e81150d606cfc4e54bd8f1552e2e3b90d579ced3
Julio Turolla <j@julioturolla.com>
2025-08-20 11:35:52 -0300

    Remove bugsgit add .git add .git add .git add .git add .git add .git add .

--- env.sh
+++ env.sh
@@ -17,15 +17,15 @@ export LC_NUMERIC="en_US.UTF-8"
 export LC_TIME="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-GPG_TTY="$(tty)"
-export GPG_TTY
+export GPG_TTY="$(tty)"
 export PINENTRY_USER_DATA="USE_CURSES=1"
 
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-GOPATH="$(go env GOPATH)"
-export GOPATH
+export GOPATH="$(go env GOPATH)"
+#export GOPRIVATE="golang.nuinfra.net"
+go env -w GOPRIVATE=github.com/nubank/*,golang.nuinfra.net/*
 
 # Set 1Password SSH agent
-export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+export SSH_AUTH_SOCK="/Users/julio.turolla/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
