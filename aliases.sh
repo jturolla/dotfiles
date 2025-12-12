b42e5d9a13b2533952670944de95bae520cffc5e
Julio Turolla <j@julioturolla.com>
2025-12-12 11:41:10 -0300

    alias vim

--- aliases.sh
+++ aliases.sh
@@ -19,3 +19,4 @@ alias flushdns='sudo killall -HUP mDNSResponder'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 alias aws-local="aws --profile local-minio --endpoint-url http://192.168.1.217:9000"
+alias vim=nvim
